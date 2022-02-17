d47f38b51d8febd8715e3eb37db5bcfff75e2a3d
SPIRAL\Gaetan <gaetan@rhinox.training>
2022-02-17 16:01:14 +0100

    Test for improved refactor detection

---
+++ Assets/Lightspeed/Core/Attributes/RefactoringOldNamespaceAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rhinox.Lightspeed
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class RefactoringOldNamespaceAttribute : Attribute
+    {
+        public string PreviousNamespace { get; }
+        
+        public RefactoringOldNamespaceAttribute(string namespaceString)
+        {
+            PreviousNamespace = namespaceString;
+        }
+    }
+}
---
+++ Assets/Lightspeed/Core/Reflection/RefactorNamespacesTypeResolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Rhinox.Lightspeed.Reflection
+{
+    public class CheckForRefactoredTypes : ICustomTypeResolver
+    {
+        private static Dictionary<string, Type> _refactoredTypesAttributes;
+        
+        public bool CheckForType(string typeName, out Type foundType)
+        {
+            if (_refactoredTypesAttributes == null)
+            {
+                _refactoredTypesAttributes = new Dictionary<string, Type>();
+                foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    foreach (var type in assembly.GetTypes())
+                    {
+                        if (type.HasCustomAttribute<RefactoringOldNamespaceAttribute>())
+                        {
+                            var attr = type.GetCustomAttribute<RefactoringOldNamespaceAttribute>();
+                            string oldName = $"{attr.PreviousNamespace}.{type.Name}";
+                            string newName = type.FullName;
+                            _refactoredTypesAttributes.Add(oldName, type);
+                        }
+                    }
+                }
+            }
+
+            foundType = _refactoredTypesAttributes.GetOrDefault(typeName);
+            return foundType != null;
+        }
+    }
+}
