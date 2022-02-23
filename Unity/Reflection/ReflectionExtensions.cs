c15b16629f57ddd7438c7b70b51d0636a112fd0d
SPIRALGaetan <gaetan@rhinox.training>
2022-02-23 13:41:03 +0100

    Moved to core

--- Unity/Reflection/ReflectionExtensions.cs
+++ Unity/Reflection/ReflectionExtensions.cs
@@ -10,41 +10,5 @@ namespace Rhinox.Lightspeed.Reflection
         {
             return memberInfo.GetReturnType().InheritsFrom(typeof(UnityEngine.Object));
         }
-
-        public static bool HasParameters(this MethodInfo methodInfo, IList<Type> paramTypes, bool inherit = true)
-        {
-            var parameters = methodInfo.GetParameters();
-            if (parameters.Length != paramTypes.Count)
-                return false;
-            for (int i = 0; i < parameters.Length; ++i)
-            {
-                if (!inherit)
-                {
-                    if (parameters[i].ParameterType != paramTypes[i])
-                        return false;
-                }
-                else
-                {
-                    if (!paramTypes[i].InheritsFrom(parameters[i].ParameterType))
-                        return false;
-                }
-            }
-
-            return true;
-        }
-        
-        public static bool IsDefined<T>(this ICustomAttributeProvider member) where T : Attribute => member.IsDefined<T>(false);
-        
-        public static bool IsDefined<T>(this ICustomAttributeProvider member, bool inherit) where T : Attribute
-        {
-            try
-            {
-                return member.IsDefined(typeof (T), inherit);
-            }
-            catch
-            {
-                return false;
-            }
-        }
     }
 }
