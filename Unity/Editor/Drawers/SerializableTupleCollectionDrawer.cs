ef3623f8d8b44612ea63d141290a3f77ac05423e
Jorian Baeke <jorian@rhinox.be>
2022-03-09 17:18:46 +0100

    Fixes GuiUtils dependency & removal GuiUtil's Attributes (You cannot use them without asmdef when odin is included)

--- Unity/Editor/Drawers/SerializableTupleCollectionDrawer.cs
+++ Unity/Editor/Drawers/SerializableTupleCollectionDrawer.cs
@@ -1,58 +1,66 @@
 using Rhinox.Lightspeed.Collections;
 using UnityEngine;
-
 using UnityEditor;
 using UnityEditorInternal;
 
-[CustomPropertyDrawer(typeof(SerializableTupleCollection), true)]
-public class SerializableTupleCollectionDrawer : PropertyDrawer 
+namespace Rhinox.Lightspeed.Editor
 {
-    private ReorderableList _list;
+    [CustomPropertyDrawer(typeof(SerializableTupleCollection), true)]
+    public class SerializableTupleCollectionDrawer : PropertyDrawer
+    {
+        private ReorderableList _list;
 
-    public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
-        if (_list == null) {
-            var listProp = property.FindPropertyRelative("list");
-            _list = new ReorderableList(property.serializedObject, listProp, true, false, true, true);
-            _list.drawElementCallback = DrawListItems;
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (_list == null)
+            {
+                var listProp = property.FindPropertyRelative("list");
+                _list = new ReorderableList(property.serializedObject, listProp, true, false, true, true);
+                _list.drawElementCallback = DrawListItems;
+            }
+
+            var firstLine = position;
+            firstLine.height = EditorGUIUtility.singleLineHeight;
+            EditorGUI.PropertyField(firstLine, property, label);
+
+            if (property.isExpanded)
+            {
+                position.y += firstLine.height;
+
+                var prevLabelWidth = EditorGUIUtility.labelWidth;
+                EditorGUIUtility.labelWidth = 80;
+
+                _list.DoList(position);
+
+                EditorGUIUtility.labelWidth = prevLabelWidth;
+            }
         }
 
-        var firstLine = position;
-        firstLine.height = EditorGUIUtility.singleLineHeight;
-        EditorGUI.PropertyField(firstLine, property, label);
+        void DrawListItems(Rect rect, int index, bool isActive, bool isFocused)
+        {
+            SerializedProperty element = _list.serializedProperty.GetArrayElementAtIndex(index); // The element in the list
 
-        if (property.isExpanded) {
-            position.y += firstLine.height;
+            var keyProp = element.FindPropertyRelative("Item1");
+            var contents = new GUIContent[] {GUIContent.none, new GUIContent(" => ")};
 
-            var prevLabelWidth = EditorGUIUtility.labelWidth;
-            EditorGUIUtility.labelWidth = 80;
+            EditorGUI.MultiPropertyField(rect, contents, keyProp,
+                EditorGUI.BeginProperty(rect, new GUIContent($"Element {index}"), element));
 
-            _list.DoList(position);
-
-            EditorGUIUtility.labelWidth = prevLabelWidth;
+            EditorGUI.EndProperty();
         }
-    }
 
-    void DrawListItems(Rect rect, int index, bool isActive, bool isFocused) {
-        SerializedProperty element = _list.serializedProperty.GetArrayElementAtIndex(index); // The element in the list
-
-        var keyProp  = element.FindPropertyRelative("Item1");
-        var contents = new GUIContent[] {GUIContent.none, new GUIContent (" => ")};
-
-        EditorGUI.MultiPropertyField(rect, contents, keyProp, EditorGUI.BeginProperty(rect, new GUIContent($"Element {index}"), element));
-
-        EditorGUI.EndProperty();
-    }
-
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
-        if (property.isExpanded) {
-            var listProp = property.FindPropertyRelative("list");
-            if (listProp.arraySize < 2)
-                return EditorGUIUtility.singleLineHeight + 42f;
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            if (property.isExpanded)
+            {
+                var listProp = property.FindPropertyRelative("list");
+                if (listProp.arraySize < 2)
+                    return EditorGUIUtility.singleLineHeight + 42f;
+                else
+                    return EditorGUIUtility.singleLineHeight + 21f * (listProp.arraySize + 1);
+            }
             else
-                return EditorGUIUtility.singleLineHeight + 21f * (listProp.arraySize + 1);
+                return EditorGUIUtility.singleLineHeight;
         }
-        else
-            return EditorGUIUtility.singleLineHeight;
     }
 }
-
