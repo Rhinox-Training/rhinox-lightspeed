c9c1b15befb71b3a2c34b2b9d4f5932f136c6cbe
Jorian Baeke <jorian@rhinox.be>
2022-03-09 15:58:03 +0100

    minor

--- Assets/Lightspeed/Unity/Helpers/Utility.RuntimeEditor.cs
+++ Assets/Lightspeed/Unity/Helpers/Utility.RuntimeEditor.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 using Object = UnityEngine.Object;
 #if UNITY_EDITOR
 using UnityEditor;
+#if UNITY_2021_2_OR_NEWER
+using UnityEditor.SceneManagement;
+#else
 using UnityEditor.Experimental.SceneManagement;
+#endif
 #endif
 
 namespace Rhinox.Lightspeed
