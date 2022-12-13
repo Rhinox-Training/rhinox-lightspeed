7a5878a53794cccce5ffcdf5e4f73fc25722e904
Gaetan Deglorie <gaetan@rhinox.training>
2022-12-13 11:12:24 +0100

    [ADD] DelegateUtility + TypeExtensions.GetNameWithNesting

---
+++ Assets/Lightspeed/Core/Extensions/TypeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Rhinox.Lightspeed
+{
+    public static class TypeExtensions
+    {
+        public static string GetNameWithNesting(this Type type)
+        {
+            if (type.IsNested && !type.IsGenericParameter)
+                return GetNameWithNesting(type.DeclaringType) + "." + type.Name;
+            return type.Name;
+        }
+    }
+}
