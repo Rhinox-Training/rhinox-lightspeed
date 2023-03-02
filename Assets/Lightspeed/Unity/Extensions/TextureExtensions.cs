e602e97918bda072f47d188ba1749cc4c74197de
Jorian Baeke <jorian@rhinox.training>
2023-03-02 13:50:11 +0100

    [ADD] MakeSquare method with a size param

--- Assets/Lightspeed/Unity/Extensions/TextureExtensions.cs
+++ Assets/Lightspeed/Unity/Extensions/TextureExtensions.cs
@@ -16,7 +16,27 @@ namespace Rhinox.Lightspeed
             if (tex == null)
                 return tex;
             
-            Texture2D paddedTex = new Texture2D(1, 1, tex.format, tex.mipmapCount > 0) {
+            var paddedTex = CreateTextureBasedOn(tex);
+            
+            paddedTex.Resize(tex.width + padding.left + padding.right, tex.height + padding.top + padding.bottom);
+            paddedTex.Apply();
+            
+            Graphics.CopyTexture(
+                tex, 0, 0, 0, 0, tex.width, tex.height,
+                paddedTex, 0, 0, padding.left, padding.top
+            );
+
+            // Not sure WHY this is needed but unfortunately the color does not properly scale (?)
+            InsetBorder(paddedTex, padding, Color.clear);
+
+            paddedTex.Apply(true);
+            return paddedTex;
+        }
+
+        private static Texture2D CreateTextureBasedOn(Texture2D tex)
+        {
+            Texture2D paddedTex = new Texture2D(1, 1, tex.format, tex.mipmapCount > 0)
+            {
 #if UNITY_EDITOR
                 alphaIsTransparency = tex.alphaIsTransparency,
 #endif
@@ -27,21 +47,10 @@ namespace Rhinox.Lightspeed
                 filterMode = tex.filterMode,
                 anisoLevel = tex.anisoLevel
             };
-
+            
             var color = Color.clear;
             paddedTex.SetPixel(0, 0, color);
-            paddedTex.Resize(tex.width + padding.left + padding.right, tex.height + padding.top + padding.bottom);
-            paddedTex.Apply();
-            
-            Graphics.CopyTexture(
-                tex, 0, 0, 0, 0, tex.width, tex.height,
-                paddedTex, 0, 0, padding.left, padding.top
-            );
-
-            // Not sure WHY this is needed but unfortunately the color does not properly scale (?)
-            InsetBorder(paddedTex, padding, color);
-
-            paddedTex.Apply(true);
+            paddedTex.Apply(false);
             return paddedTex;
         }
 
@@ -55,6 +64,13 @@ namespace Rhinox.Lightspeed
                 offset = new RectOffset(Mathf.CeilToInt(halfDiff), Mathf.FloorToInt(halfDiff), 0, 0);
             
             return tex.Pad(offset);
+        }
+        
+        public static Texture2D MakeSquare(this Texture2D tex, int size)
+        {
+            var newTex = tex.MakeSquare();
+            newTex.Resize(size, size);
+            return newTex;
         }
 
         public static void InsetBorder(this Texture2D tex, RectOffset border, Color color)
