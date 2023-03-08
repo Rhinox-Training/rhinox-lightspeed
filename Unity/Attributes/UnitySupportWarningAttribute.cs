f8ff838b78ecf65ed9e1cb67e641a283af4a23bb
Jorian Baeke <jorian@rhinox.training>
2023-03-08 17:16:54 +0100

    [MOD] Make it a PropertyAttribute

--- Unity/Attributes/UnitySupportWarningAttribute.cs
+++ Unity/Attributes/UnitySupportWarningAttribute.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using UnityEngine;
 
 /// <summary>
 /// Add this attribute to a class that requires a certain version of unity to function.
 /// The given version is the minimum version required.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = true)]
-public class UnitySupportWarningAttribute : Attribute
+public class UnitySupportWarningAttribute : PropertyAttribute
 {
     public int Major;
     public int Minor;
