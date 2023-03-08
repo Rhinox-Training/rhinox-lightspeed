b05091f8d3ad5c3fd4c0e61396003722631af91e
Jorian Baeke <jorian@rhinox.training>
2023-03-08 17:13:07 +0100

    [ADD] UnitySupportWarning attribute, to indicate certain versions not being supported

---
+++ Assets/Lightspeed/Unity/Attributes/UnitySupportWarningAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Add this attribute to a class that requires a certain version of unity to function.
+/// The given version is the minimum version required.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = true)]
+public class UnitySupportWarningAttribute : Attribute
+{
+    public int Major;
+    public int Minor;
+
+    public UnitySupportWarningAttribute(int major, int minor = 0)
+    {
+        Major = major;
+        Minor = minor;
+    }
+}
---
+++ Assets/Tests/FieldTester.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Rhinox.Lightspeed;
+using Rhinox.Lightspeed.Collections;
+using UnityEngine;
+
+
+public class FieldTester : MonoBehaviour
+{
+    [SerializeReference]
+    public CustomCollection<string> Test;
+
+    [SerializeReference]
+    public SceneReference Scene;
+    
+    
+}
