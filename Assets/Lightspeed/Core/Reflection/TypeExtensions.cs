03ff54db12b0e7c9720ace59cc3bfee96afdf21d
Gaetan Deglorie <gaetan@rhinox.training>
2023-03-29 15:43:29 +0200

    [MOD] ReflectionExtensions, TypeExtensions, SerializeHelper

--- Assets/Lightspeed/Core/Reflection/TypeExtensions.cs
+++ Assets/Lightspeed/Core/Reflection/TypeExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Rhinox.Lightspeed.Reflection
 {
@@ -182,6 +183,73 @@ namespace Rhinox.Lightspeed.Reflection
                 return genericImplementations[0];
 
             throw new ArgumentException("Not a collection type", nameof(collectionType));
+        }
+        
+        public static string GetCSharpName(this Type type, Stack<Type> genericArgs = null, StringBuilder arrayBrackets = null)
+        {
+            StringBuilder code = new StringBuilder();
+            Type declaringType = type.DeclaringType;
+
+            bool arrayBracketsWasNull = arrayBrackets == null;
+
+            if (genericArgs == null)
+                genericArgs = new Stack<Type>(type.GetGenericArguments());
+
+
+            int currentTypeGenericArgsCount = genericArgs.Count;
+            if (declaringType != null)
+                currentTypeGenericArgsCount -= declaringType.GetGenericArguments().Length;
+
+            Type[] currentTypeGenericArgs = new Type[currentTypeGenericArgsCount];
+            for (int i = currentTypeGenericArgsCount - 1; i >= 0; i--)
+                currentTypeGenericArgs[i] = genericArgs.Pop();
+
+
+            if (declaringType != null)
+                code.Append(GetCSharpName(declaringType, genericArgs)).Append('.');
+
+
+            if (type.IsArray)
+            {
+                if (arrayBrackets == null)
+                    arrayBrackets = new StringBuilder();
+
+                arrayBrackets.Append('[');
+                arrayBrackets.Append(',', type.GetArrayRank() - 1);
+                arrayBrackets.Append(']');
+
+                Type elementType = type.GetElementType();
+                code.Insert(0, GetCSharpName(elementType, arrayBrackets: arrayBrackets));
+            }
+            else
+            {
+                code.Append(new string(type.Name.TakeWhile(c => char.IsLetterOrDigit(c) || c == '_').ToArray()));
+
+                if (currentTypeGenericArgsCount > 0)
+                {
+                    code.Append('<');
+                    for (int i = 0; i < currentTypeGenericArgsCount; i++)
+                    {
+                        code.Append(GetCSharpName(currentTypeGenericArgs[i]));
+                        if (i < currentTypeGenericArgsCount - 1)
+                            code.Append(',');
+                    }
+
+                    code.Append('>');
+                }
+
+                if (declaringType == null && !string.IsNullOrEmpty(type.Namespace))
+                {
+                    code.Insert(0, '.').Insert(0, type.Namespace);
+                }
+            }
+
+
+            if (arrayBracketsWasNull && arrayBrackets != null)
+                code.Append(arrayBrackets.ToString());
+
+
+            return code.ToString();
         }
     }
 }
