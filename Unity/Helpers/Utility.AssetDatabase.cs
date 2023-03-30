208ddaf5cd2ce0fc2e28207cce98be0d02707552
Jorian Baeke <jorian@rhinox.training>
2023-03-30 15:08:15 +0200

    [ADD] Utility.Assetdatabase + moved methods from AssetExtensions to under Utility

--- Unity/Helpers/Utility.AssetDatabase.cs
+++ Unity/Helpers/Utility.AssetDatabase.cs
@@ -1,24 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
 namespace Rhinox.Lightspeed
 {
-#if UNITY_EDITOR
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using UnityEditor;
-    using UnityEngine;
-
-    public static class AssetExtensions
+    public static partial class Utility
     {
+#if UNITY_EDITOR
+        public static T FindApproximately<T>(params string[] possibleNames) where T : UnityEngine.Object
+        {
+            foreach (var name in possibleNames)
+            {
+                string[] guids = AssetDatabase.FindAssets(name);
+                foreach (var guid in guids)
+                {
+                    string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                    // NOTE: Unity is stupid, so we need to load every variant as a potential typed instance to check if it is of a certain type
+                    var potentialInstance = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+                    if (potentialInstance != null)
+                    {
+                        return potentialInstance;
+                    }
+                }
+            }
+
+            return null;
+        }
+
         public static T[] FindPrefabsOfType<T>(params string[] folders) where T : MonoBehaviour
         {
-            //AssetDatabase.Refresh();
             if (folders == null)
                 folders = Array.Empty<string>();
+
             if (folders.Length > 0)
                 folders = folders.Where(x => x != null).ToArray();
-            string[] guids = AssetDatabase.FindAssets("t:Object", folders.Length == 0 ? new[] { "Assets" } : folders);
 
             List<T> prefabMonoBehaviours = new List<T>();
+
+            //AssetDatabase.Refresh();
+
+            string[] guids = AssetDatabase.FindAssets("t:Object", folders.Length == 0 ? new[] { "Assets" } : folders);
+
             foreach (string guid in guids)
             {
                 //Debug.Log(AssetDatabase.GUIDToAssetPath(guid));
@@ -39,12 +63,12 @@ namespace Rhinox.Lightspeed
 
             return prefabMonoBehaviours.ToArray();
         }
-    
+
         public static T[] FindScriptableObjectsOfType<T>(params string[] folders) where T : ScriptableObject
         {
             //AssetDatabase.Refresh();
             string[] assetGUIDs = AssetDatabase.FindAssets($"t:{typeof(T).FullName}", folders.Length == 0 ? new[] { "Assets" } : folders);
-        
+
             List<T> scriptableObjects = new List<T>();
             foreach (string guid in assetGUIDs)
             {
@@ -52,12 +76,13 @@ namespace Rhinox.Lightspeed
                 T asset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
                 if (asset == null)
                     continue;
-            
+
                 scriptableObjects.Add(asset);
             }
-        
+
             return scriptableObjects.ToArray();
         }
-    }
+
 #endif
+    }
 }
