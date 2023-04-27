bba16743da86ab635c68443684c0d567683ea0b4
Gaetan Deglorie <gaetan@rhinox.training>
2023-04-27 14:08:53 +0200

    [MOD] Created new utility file for collections utilities

--- Core/Helpers/Utility.cs
+++ Core/Helpers/Utility.cs
@@ -67,28 +67,5 @@ namespace Rhinox.Lightspeed
             }
             return s[0].Substring(0, k);
         }
-
-        private static MethodInfo _arrayResizeStaticMethod;
-        private static object[] _resizeArrayParameters;
-        public static object ResizeArrayGeneric(object array, int newSize)
-        {
-            var type = array.GetType();
-            if (!type.IsArray)
-                throw new ArgumentException($"array was of type {type.GetNiceName()}, expected ArrayType");
-            
-            var elemType = type.GetElementType();
-            if (_arrayResizeStaticMethod == null)
-                _arrayResizeStaticMethod = typeof(Array).GetMethod("Resize", BindingFlags.Static | BindingFlags.Public);
-            
-            var properResizeMethod = _arrayResizeStaticMethod.MakeGenericMethod(elemType);
-            if (_resizeArrayParameters == null)
-                _resizeArrayParameters = new object[2];
-            _resizeArrayParameters[0] = array;
-            _resizeArrayParameters[1] = newSize;
-            
-            properResizeMethod.Invoke(null, _resizeArrayParameters);
-            array = _resizeArrayParameters[0];
-            return array;
-        }
     }
 }
