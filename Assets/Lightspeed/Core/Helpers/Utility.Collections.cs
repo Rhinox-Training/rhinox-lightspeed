fb1c836ea1595de2752ede5bcbe639eaa2f53fbc
Gaetan Deglorie <gaetan@rhinox.training>
2023-04-27 14:08:53 +0200

    [MOD] Created new utility file for collections utilities

---
+++ Assets/Lightspeed/Core/Helpers/Utility.Collections.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using Rhinox.Lightspeed.Reflection;
+
+namespace Rhinox.Lightspeed
+{
+    public partial class Utility
+    {
+        private static MethodInfo _arrayResizeStaticMethod;
+        private static object[] _resizeArrayParameters;
+        public static object ResizeArrayGeneric(object array, int newSize)
+        {
+            var type = array.GetType();
+            if (!type.IsArray)
+                throw new ArgumentException($"array was of type {type.GetNiceName()}, expected ArrayType");
+            
+            var elemType = type.GetElementType();
+            if (_arrayResizeStaticMethod == null)
+                _arrayResizeStaticMethod = typeof(Array).GetMethod("Resize", BindingFlags.Static | BindingFlags.Public);
+            
+            var properResizeMethod = _arrayResizeStaticMethod.MakeGenericMethod(elemType);
+            if (_resizeArrayParameters == null)
+                _resizeArrayParameters = new object[2];
+            _resizeArrayParameters[0] = array;
+            _resizeArrayParameters[1] = newSize;
+            
+            properResizeMethod.Invoke(null, _resizeArrayParameters);
+            array = _resizeArrayParameters[0];
+            return array;
+        }
+    }
+}
