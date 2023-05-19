d7bc4ab0b6543b0901665e53d2c665a8e9f5d1b5
Gaetan Deglorie <gaetan@rhinox.training>
2023-05-19 12:22:16 +0200

    [ADD] Orthogonal Plane

---
+++ Assets/Lightspeed/Unity/Data/Geometry/OrthogonalPlane.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Rhinox.Lightspeed
+{
+    public struct OrthogonalPlane
+    {
+        public Axis Axis;
+        public Vector3 Point;
+
+        public OrthogonalPlane(Axis axis, Vector3 point)
+        {
+            if (axis.IsSingleFlag()) throw new ArgumentOutOfRangeException(nameof(axis));
+            Axis = axis;
+            Point = point;
+        }
+
+        public Plane ToPlane()
+        {
+            switch (Axis)
+            {
+                case Axis.X:
+                    return new Plane(Vector3.right, Point);
+                case Axis.Y:
+                    return new Plane(Vector3.up, Point);
+                case Axis.Z:
+                    return new Plane(Vector3.forward, Point);
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
