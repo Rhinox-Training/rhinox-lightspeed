677b5237489d791fe679fb98f7612825a8733e94
Gaetan Deglorie <gaetan@rhinox.training>
2023-12-19 10:04:40 +0100

    [ADD] LayerUtils

---
+++ Unity/Helpers/LayerUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Rhinox.GUIUtils.Utils
+{
+    public static class LayerUtils
+    {
+        public static string[] GetLayerNames()
+        {
+            //there are only 32 layer fields in Unity
+            return Enumerable.Range(0, 32)
+                             .Select(index => LayerMask.LayerToName(index))
+                             .Where(l => !string.IsNullOrEmpty(l))
+                             .ToArray();
+        }
+    }
+}
